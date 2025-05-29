9e50e42d96fbe3db5f3d1e159bf9add5b4bbc234
Matipala <josue.molina@ucb.edu.bo>
2025-05-29 18:39:12 -0400

    update en la seccion del mockdata projectcomponent

--- Portafolio/components/projects-component.js
+++ Portafolio/components/projects-component.js
@@ -144,6 +144,17 @@ class ProjectsComponent extends HTMLElement {
         </div>
       </section>
     `;
+    this.shadowRoot.querySelectorAll('.portfolio__item').forEach(item => {
+      item.addEventListener('click', () => {
+        const index = item.getAttribute('data-index');
+        const selectedProject = projects[index];
+        this.dispatchEvent(new CustomEvent('project-selected', {
+          detail: selectedProject,
+          bubbles: true,
+          composed: true
+        }));
+      });
+    });
   }
 }
 customElements.define('projects-section', ProjectsComponent);
