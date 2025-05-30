add7bd6fe4b862e94e793272d237835b659da4a3
Matipala <josue.molina@ucb.edu.bo>
2025-05-30 19:38:54 -0400

    correcciones de storage

--- Portafolio/utils/storage.js
+++ Portafolio/utils/storage.js
@@ -1,8 +1,18 @@
-export const savetoStorage = (key, value) => {
+export const saveToStorage = (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
 };
 
 export const loadFromStorage = (key) => {
-    const value = localStorage.getItem(key);
-    return value ? JSON.parse(value) : [];
+    const json = localStorage.getItem(key);
+    if (!json) return null;
+    try {
+        return JSON.parse(json);
+    } catch (error) {
+        console.error('Error parsing JSON from localStorage for key', key);
+        return null;
+    }
+};
+
+export const removeFromStorage = (key) => {
+    localStorage.removeItem(key);
 };
